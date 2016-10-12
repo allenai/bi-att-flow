3b67210e2a66a04bf407c2953d50140d2b1ad0b8
Minjoon Seo <seominjoon@gmail.com>
2016-10-12 15:32:59 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -14,7 +14,7 @@ wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_
 GLOVE_DIR=$DATA_DIR/glove
 mkdir $GLOVE_DIR
 wget http://nlp.stanford.edu/data/glove.6B.zip -O $GLOVE_DIR/glove.6B.zip
-unzip $GLOVE_DIR/glove.6B.zip
+unzip $GLOVE_DIR/glove.6B.zip -d $GLOVE_DIR
 
 # Download NLTK (for tokenizer)
 # Make sure that nltk is installed!
