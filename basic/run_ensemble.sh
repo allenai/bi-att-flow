baeac016a483ccbc0d444fcb6fe3fd968d841b8c
Minjoon Seo <seominjoon@gmail.com>
2016-11-27 11:08:38 -0800

    typo in run_ensemble.sh fixed

--- basic/run_ensemble.sh
+++ basic/run_ensemble.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source_path=$1
 target_path=$2
-inter_dir="inter_single"
+inter_dir="inter_ensemble"
 root_dir="save"
 
 parg=""
@@ -20,7 +20,7 @@ for num in 31 33 34 35 36 37 40 41 43 44 45 46; do
     load_path="$root_dir/$num/save"
     shared_path="$root_dir/$num/shared.json"
     eval_path="$inter_dir/eval-$num.json"
-    eargs="$args $eval_path"
+    eargs="$eargs $eval_path"
     python3 -m basic.cli --data_dir $inter_dir --eval_path $eval_path --nodump_answer --load_path $load_path --shared_path $shared_path $marg --eval_num_batches 0 --mode forward --batch_size 1 --len_opt --cluster --cpu_opt --load_ema &
 done
 wait
