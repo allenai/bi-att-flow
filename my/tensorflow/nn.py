3fb0943fefd71eea6886dd247d373f46054e922f
pcgreat <chao@wayblazer.com>
2017-06-29 12:21:32 -0500

    compatible with tensorflow v1.2.0

--- my/tensorflow/nn.py
+++ my/tensorflow/nn.py
@@ -1,4 +1,4 @@
-from tensorflow.contrib.rnn.python.ops.core_rnn_cell_impl import _linear
+from tensorflow.python.ops.rnn_cell_impl import _linear
 from tensorflow.python.util import nest
 import tensorflow as tf
 
@@ -18,7 +18,7 @@ def linear(args, output_size, bias, bias_start=0.0, scope=None, squeeze=False, w
         flat_args = [tf.cond(is_train, lambda: tf.nn.dropout(arg, input_keep_prob), lambda: arg)
                      for arg in flat_args]
     with tf.variable_scope(scope or 'Linear'):
-        flat_out = _linear(flat_args, output_size, bias, bias_start=bias_start)
+        flat_out = _linear(flat_args, output_size, bias, bias_initializer=tf.constant_initializer(bias_start))
     out = reconstruct(flat_out, args[0], 1)
     if squeeze:
         out = tf.squeeze(out, [len(args[0].get_shape().as_list())-1])
