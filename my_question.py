66fac8f019f9fe67173f307b271e5fa017c1e78f
Sewon Min <shmsw25@snu.ac.kr>
2018-01-26 23:19:13 -0800

    add some interesting questions

---
+++ my_question.py
@@ -0,0 +1,15 @@
+
+my_question = {
+	1:	["How far is the town that Chopin was born from Warsaw?",
+		"How far is the town that Chopin was born from Warsaw in km?",
+		"How far is the town that Chopin was born from Warsaw in mile?"],
+	6:	["What is the German name of Martin Luther?"],
+	9:	["Which country is the University of Melbourne located in?"],
+	13:	["The 1973 oil crisis was also called which name later?"],
+	18: 	["In which state is the 34th-largest city in the United States located?"],
+	22:	["When did the Newcastle lose its independence?"],
+	40:	["Which positive number is neither prime nor composite?"],
+	41:	["What is the longest river in Central and Western Europe?"],
+	46:	["Where did the French and Indian War take place? => North American theater"]
+}
+
